00142e593559474b724a0e263373e2e88dec2a1d
Feraklin <feraklin@ya.ru>
2021-11-25 00:44:54 +0300

    add class Drawing_plots

--- plots_generation.py
+++ plots_generation.py
@@ -1,18 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+class Drawing_plots():
+    def __init__(self, colomn, average_line=False):
+        self.colomn = colomn
+        self.average_line = average_line
 
-def drawing_plots(colomn, average_line=False):
-    data = pd.read_json("deviations.json")
+    def drawing_plots(self):
+        data = pd.read_json("deviations.json")
 
-    fig, ax = plt.subplots()
-    ax.plot(data[colomn], color="red", label=colomn)
-    if average_line == True:
-        plt.axhline(
-            y=data[colomn].mean(), color="blue", linestyle="--", label="average"
-        )
-    ax.set(xlabel="Object", ylabel="Angle", title=(colomn + " Deviation angles"))
-    ax.legend(loc="upper right")
-    fig.savefig("plots/" + colomn + " deviations.png")
-    return "plots/" + colomn + " deviations.png"
-
+        fig, ax = plt.subplots()
+        ax.plot(data[self.colomn], color="red", label=self.colomn)
+        if self.average_line == True:
+            plt.axhline(
+                y=data[self.colomn].mean(), color="blue", linestyle="--", label="average"
+            )
+        ax.set(xlabel="Object", ylabel="Angle", title=(self.colomn + " Deviation angles"))
+        ax.legend(loc="upper right")
+        fig.savefig("plots/" + self.colomn + " deviations.png")
+        return "plots/" + self.colomn + " deviations.png"
