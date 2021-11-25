1e166dc6990bee3054a6a10b2fd78aefb32e21bf
Feraklin <feraklin@ya.ru>
2021-11-25 13:10:24 +0300

    add multiply data plot

--- plots_generation.py
+++ plots_generation.py
@@ -2,22 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class Drawing_plots():
-    def __init__(self, colomn, average_line=False):
+    def __init__(self, plot_type, colomn, average_line=False):
         self.colomn = colomn
         self.average_line = average_line
+        self.plot_type = plot_type
 
     def drawing_plots(self):
+        color_array = ["red", "blue", "green","black"]
         data = pd.read_json("deviations.json")
 
         fig, ax = plt.subplots()
-        ax.scatter(range(0,len(data)), data[self.colomn], color="red", label=self.colomn, s=5)
-        if self.average_line == True:
-            plt.axhline(
-                y=data[self.colomn].mean(), color="blue", linestyle="--", label="average"
-            )
-        ax.set(xlabel="Object", ylabel="Angle", title=(self.colomn + " Deviation angles"))
+        for i in range(0,len(self.colomn)):
+            if self.plot_type == "scatter":
+                ax.scatter(range(0,len(data)), data[self.colomn[i]], c=color_array[i], label=self.colomn[i], s=3)
+                if self.average_line == True:
+                    plt.axhline(
+                        y=data[self.colomn[i]].mean(), color=color_array[i], linestyle="--", label=("average " +self.colomn[i]) 
+                    )
+                ax.set(xlabel="Object", ylabel="Angle", title= (' '.join([i for i in self.colomn]) + " Deviation angles"))
+        
+        
         ax.legend(loc="upper right")
-        fig.savefig("plots/" + self.colomn + " deviations.png")
-        return "plots/" + self.colomn + " deviations.png"
+        file_name = "plots/" + ' '.join([i for i in self.colomn]) + " deviations.png"
+        fig.savefig(file_name)
+        return file_name
 
-Drawing_plots("mean",average_line = False).drawing_plots()
+Drawing_plots("scatter", ("min","max"),average_line = True).drawing_plots()
